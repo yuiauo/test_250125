13c5f9623181aa877aefa8f70980b195136130f2
Yuri Bakurov <yuri.bakurov@arsis.ru>
2025-01-26 03:38:17 +0300

    fix: removed filehandler for logger

--- app_logger.py
+++ app_logger.py
@@ -6,7 +6,5 @@ logger = logging.getLogger(__name__)
 logging.basicConfig(
     level=logging.INFO,
     format='%(levelname)s: %(message)s',
-    handlers=[
-        logging.StreamHandler(sys.stdout),
-        logging.FileHandler('logs/logs.log')],
+    handlers=[logging.StreamHandler(sys.stdout)]
 )
